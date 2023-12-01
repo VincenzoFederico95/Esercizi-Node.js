4ba30f4f4ef1ae63396f88f8a3e490cca15ceef2
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-12-01 17:38:05 +0100

    11

---
+++ esercizio11.js
@@ -0,0 +1,37 @@
+/* The newsEvent object continuously emits three different events: newsEvent, breakingNews and error
+
+Attach event listeners for each event and log out their data. */
+
+const { EventEmitter } = require("node:events");
+
+function createNewsFeed() {
+  const emitter = new EventEmitter();
+
+  emitter.on("newsEvent", (news) => {
+    console.log(news);
+  });
+
+  emitter.on("breakingNews", (breakingNews) => {
+    console.log(breakingNews);
+  });
+
+  emitter.on("error", (error) => {
+    console.error(error);
+  });
+
+  setInterval(() => {
+    emitter.emit("newsEvent", "News: A thing happened in a place.");
+  }, 1000);
+
+  setInterval(() => {
+    emitter.emit("breakingNews", "Breaking news! A BIG thing happened.");
+  }, 4000);
+
+  setTimeout(() => {
+    emitter.emit("error", new Error("News feed connection error"));
+  }, 5000);
+
+  return emitter;
+}
+
+const newsFeed = createNewsFeed();
