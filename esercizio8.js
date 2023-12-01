c3e548f31197f135f2c30b93160dfca46b90dfbe
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-12-01 17:29:37 +0100

    8

--- esercizio8.js
+++ esercizio8.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.writeFile("./text8.txt", "Questo è un file txt creato con Node.js", (err, data) => {
+fs.writeFile("./text8.txt", "Ciao a tutti! creato con Node.js", (err, data) => {
     if (err) {
         console.log(err);
     }
